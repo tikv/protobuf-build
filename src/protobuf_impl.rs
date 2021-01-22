cdf070827463f43ed79d9ed431bcc0ef41fda30f
Stepan Henek <stepan@henek.name>
2021-01-22 22:32:37 +0100

    Take care of deprecated clippy warning
    Signed-off-by: Stepan Henek <stepan@henek.name>

--- src/protobuf_impl.rs
+++ src/protobuf_impl.rs
@@ -80,7 +80,7 @@ impl Builder {
 
         let desc_bytes = std::fs::read(&desc_file).unwrap();
         let desc: protobuf::descriptor::FileDescriptorSet =
-            protobuf::parse_from_bytes(&desc_bytes).unwrap();
+            protobuf::Message::parse_from_bytes(&desc_bytes).unwrap();
         let mut files_to_generate = Vec::new();
         'outer: for file in &self.files {
             for include in &self.includes {
